01ffbd61cf769fa046e5591f879541c5a3e1d0b6
TAL <truonglybach@gmail.com>
2018-12-03 19:32:52 -0800

    Added style and changes to app

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# project2UCI: Smoking Hot Data
+#Smoking Hot Data
+
+<hr/>
+
+### Repo Collaborators
 
 This repository houses the work of Anthony Alvarez, Daniel Evans, Sonia Kingslien, and Ly-Bach Truong.
 
+### Technologies Used
 The group utilizes several Python/Javascript libraries, such as PyMongo, pandas, Flask, D3, and React.js to import data from several CSV files into a NoSQL database (MongoDB) and serves the data to the end user through the utilization of RESTful APIs.
 
-Clients can view our visualizations of the data on the index page of the web app.
+### Web Application Info
+We use smoking data that are stored in several csv files to create the data visualizations. The web app pulls data from a MongoDB database and pushes out the data in JSON format as API endpoints.
+
+Afterwards, the JS script collects the data from those endpoints to create the visualizations.
+
+Clients can view the visualizations on the index page of the web app.
+
+<hr/>
+#### Sources
+We collected our data from the following website: 
+<kbd>https://ourworldindata.org/smoking</kbd>
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,24 @@
+// Zips for each item out of two arrays together and returns an array of objects
+function JSONify(arr1, arr2) {
+        var JSONdata = [];
+        var o;
+        arr1.forEach(function(e, i) {
+            o = arr2[i];
+            JSONdata.push({e, o});
+        })
+        return JSONdata;
+    }
+
+// Function that takes removes duplicates in an array and returns a new array
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+// Set height and width of SVGs
 var svgWidth = 1000;
 var svgHeight = 500;
 
+// Set margins
 var margin = {
     top: 30,
     right: 30,
@@ -8,36 +26,49 @@ var margin = {
     left: 30
 }
 
+// Set the height and width of the actual plot/chart
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3.select("#plot1")
+// Create the barChartSVG
+var barChartSVG = d3.select("#barChart")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
+// Create the lineChartSVG
+var lineChartSVG = d3.select("#lineChart")
+    .append("svg")
+    .attr("width", svgWidth + 40)
+    .attr("height", svgHeight);
+
+
+// Add an SVG group to each chart
+var barChartGroup = barChartSVG.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-const link = "http://127.0.0.1:5000/collections/avgpricepack";
+var lineChartGroup = lineChartSVG.append("g")
+    .attr("transform", `translate(${margin.left + 24}, ${margin.top})`);
 
-// d3.csv("../../data/avg_price_of_pack_df_2014.csv", function(error, dataCSV) {
-d3.json(link, function(dataCSV) {
-    // if (error) throw error;
+const barLink = "http://127.0.0.1:5000/collections/avgpricepack";
+const lineLink = "http://127.0.0.1:5000/collections/numdeathstobaccosmoking";
 
-    var countryArr = [];
+d3.json(barLink, function(error, barJSON) {
+    if (error) throw error;
+
+    var entityArr = [];
     var cigPricesArr = [];
-    dataCSV.forEach(function(data) {
+    barJSON.forEach(function(data) {
         if (data.Year === 2014) {
-            countryArr.push(data.Entity);
+            entityArr.push(data.Entity);
             cigPricesArr.push(+Object.values(data).slice(2, 3));
         }
     })
 
     var xScaleD = d3.scaleBand()
-        .domain(countryArr)
+        .domain(entityArr)
         .range([0, width])
-        .padding(1);
+        .padding(.1);
 
     var yScaleD = d3.scaleLinear()
         .domain([0, d3.max(cigPricesArr)])
@@ -46,7 +77,7 @@ d3.json(link, function(dataCSV) {
     var bottomAxis = d3.axisBottom(xScaleD);
     var leftAxis = d3.axisLeft(yScaleD);
 
-    var xAxis = chartGroup.append("g")
+    var xAxis = barChartGroup.append("g")
         .classed("x-axis", true)
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis)
@@ -56,34 +87,130 @@ d3.json(link, function(dataCSV) {
         .attr("dy", "-.55em")
         .attr("transform", "rotate(-45)");
 
-    var yAxis = chartGroup.append("g")
+    var yAxis = barChartGroup.append("g")
         .classed("y-axis", true)
-        .call(leftAxis)
+        .call(leftAxis);
 
-    // var objData = {"countries": countryArr,
-    //                "cigprices": cigPricesArr}
+    var objDJSON = JSONify(entityArr, cigPricesArr)
 
-    // var zippedData = countryArr.map(function(e, i) {
-    //   return [e, cigPricesArr[i]];
-    // });
-
-    // var lineArray = [];
-    // zippedData.forEach(function (infoArray, index) {
-    //     var line = infoArray.join(",");
-    //     lineArray.push(index == 0 ? "countries,cigprices \n" + line : line);
-    // });
-    // var csvContent = lineArray.join("\n");
-
-    chartGroup.selectAll(".bar")
-        .data()
+    var barChart = barChartGroup.selectAll(".bar")
+        .data(objDJSON)
         .enter()
         .append("rect")
         .attr("class", "bar")
-        .style("fill", "steelblue")
-        .attr("x", d => xScaleD(d.Entity))
-        .attr("y", d => yScaleD(d.CigPrices))
+        .style("fill", "#960A24")
+        .attr("x", d => xScaleD(d.e))
+        .attr("y", d => yScaleD(d.o))
         .attr("width", xScaleD.bandwidth())
-        .attr("height", d => height - yScaleD(d.CigPrices));
+        .attr("height", d => height - yScaleD(d.o));
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<b>Avg Price per Pack</b> (<i>in Int'l Dollars</i>)<br/><br/>${d.e}: $${d.o}`);
+        });
+
+    barChart.call(toolTip);
+
+    barChart.on("mouseover", function(data) {
+        toolTip.show(data);
+    })
+        .on("mouseout", function(data) {
+            toolTip.hide(data);
+        });
 
 })
+
+var parseTime = d3.timeParse("%Y");
+
+d3.json(lineLink, function(error, lineJSON) {
+    if (error) throw error;
+
+    var yearArr = []
+    var smokeCountArr = []
+    var entityArr = []
+
+    lineJSON.forEach(function(data) {
+        entityArr.push(data.Entity);
+
+        data.Year = parseTime(data.Year);
+        yearArr.push(data.Year);
+
+        data.smokeCount = +data.smokeCount;
+        smokeCountArr.push(+data.smokeCount);
+    })
+
+// Filter JSON based on entity/country
+
+function filterbyEntity(JSON, val) {
+    var filteredData = [];
+    JSON.forEach(function(data) {
+        if (data.Entity === val) {
+            filteredData.push(data);
+        }
+    });
+    return filteredData;
+}
+
+var uniqueEntities = entityArr.filter(onlyUnique);
+var uniqueYears = yearArr.filter(onlyUnique);
+
+for (i=0; i<uniqueEntities.length; i++) {
+    var countryName = uniqueEntities[i];
+    var countryData = filterbyEntity(lineJSON, uniqueEntities[i]);
+    // var countryYears = []
+    // var countrySC = []
+
+    // countryData.forEach(function(data) {
+    //     countryYears.push(data.Year);
+    //     countrySC.push(data.smokeCount);
+    // })
+
+    var xTimeScale = d3.scaleTime()
+        .domain(d3.extent(countryData, data => data.Year))
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(countryData, data => data.smokeCount)])
+        .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xTimeScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // !!!drawLine function not working!!!
+    var drawLine = d3.line()
+        .x(d => xTimeScale(d.Year))
+        .y(d => yLinearScale(d.smokeCount))
+
+    // lineChartGroup.append("path")
+    //     .attr("d", drawLine)
+    //     .classed("line", true);
+
+    // lineChartGroup.append("g")
+    //     .classed("axis", true)
+    //     .call(leftAxis);
+
+    // lineChartGroup.append("g")
+    //     .classed("axis", true)
+    //     .attr("transform", `translate(0, ${height})`)
+    //     .call(bottomAxis);
+
+
+}
+
+
+
+
+
+    // var objData = JSONify(yearArr, smokeCountArr);
+
+// CONTINUE HERE TO APPEND
+
+
+})
+
+
+// Add user interaction by having a text box/option thing where 
+// they can choose country to filter by and create line chart from
 
